bae352c161111d85f3d80a3d2c01e36bfddf45c7
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-10 15:17:11 +0200

    Add user validation and remove now redundant code from createUser function

---
+++ backend/src/util/validation.js
@@ -0,0 +1,50 @@
+const { body, validationResult } = require("express-validator");
+const prisma = require("../config/prisma");
+
+const validateUser = [
+    body("username")
+        .notEmpty().withMessage("Username cannot be empty")
+        .trim()
+        .isString().withMessage("Username has to be of type string")
+        .isAlphanumeric()
+        .withMessage("Username must be alpha-numeric (only numbers and letters")
+        .isLength({ min: 1, max: 25 })
+        .withMessage("Username must be between 1 and 25 characters")
+        //Check if username is already in use
+        .custom(async (value) => {
+            const user = await prisma.user.findUnique({
+                where: { username: value },
+            });
+            if (user) throw new Error("Username is already in use");
+        })
+        .withMessage("Username is already in use"),
+
+    body("email")
+        .notEmpty().withMessage("Email cannot be empty")
+        .isString().withMessage("Email has to be of type string")
+        .trim()
+        .isEmail()
+        .withMessage("Email must be in a valid email format")
+        .isLength({ min: 1, max: 50 })
+        .withMessage("Username must be between 1 and 50 characters")
+        //Check if email is already in use
+        .custom(async (value) => {
+            const user = await prisma.user.findUnique({
+                where: { email: value },
+            });
+            if (user) throw new Error("Email is already in use");
+        })
+        .withMessage("Email is already in use"),
+
+    body("password")
+        .notEmpty().withMessage("Password cannot be empty")
+        .isString().withMessage("Password has to be of type string")
+        .trim()
+        .isLength({ min: 1, max: 25 })
+        .withMessage("Password must be between 1 and 25 characters"),
+];
+
+module.exports = {
+    validationResult,
+    validateUser,
+}
