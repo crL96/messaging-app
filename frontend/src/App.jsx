bc7556e7d7b0d91c39f2b9a8641ed21f2dbb3c14
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-11 15:29:31 +0200

    Add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <h1>Hello world</h1>
+      <Link to="/login">Login</Link>
     </>
   )
 }
