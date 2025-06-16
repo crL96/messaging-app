7bb66b7e9014d984ef1320d0fa435d238925a201
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-16 14:25:47 +0200

    Format timestamp shown on messages

--- frontend/src/components/activeChat/chatMessage.jsx
+++ frontend/src/components/activeChat/chatMessage.jsx
@@ -1,15 +1,16 @@
 function ChatMessage({ message, currentUser }) {
-    
+    const formattedTime = new Date(message.timestamp).toLocaleString("en-GB");
+
     let messageClass = "recievedMessage";
     if (currentUser === message.sender.username) {
         messageClass = "sentMessage";
     }
-    
+
     return (
         <div className={messageClass}>
             <h4>{message.sender.username}</h4>
             <p>{message.text}</p>
-            <p>{message.timestamp}</p>
+            <p>{formattedTime}</p>
         </div>
     );
 }
